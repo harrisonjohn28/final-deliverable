48129036a2e08644894475d0ebbcda09c6bc090a
broyce24 <broyce@uw.edu>
2022-06-01 16:31:33 -0700

    added styling to my text

--- ui.R
+++ ui.R
@@ -86,8 +86,11 @@ genres_sidebar <- sidebarPanel(
 )
 genre_chart <- mainPanel(
   plotlyOutput(outputId = "line_chart"),
-  includeMarkdown("lineAnalysis.md")
-)
+  p(`class` = "ben",
+    "We chose this chart to analyze the public's interest in specific genres over the course of the year, as a worldwide pandemic may affect the topics people are interested in checking out. As we can see, the most popular genres stay fairly regular relative to one another, with the exception of Juvenile Fiction (YA novel), which began being checked out at an incredible rate as quarantine set in. A likely explanation for this finding is that when school was canceled for middle and high schoolers, many of them choose to check out their favorite books to read while stuck at home. Young Adult is a very popular genre with this age group and this would explain the sudden boom in checkouts.
+
+Many of the books' primary genres were labeled as fiction/nonfiction, which would've been the most popular \"genres\" by far had they been included on the graph. Because splitting up books into these two labels is vague and inconsistent (not every book had this distinction, and some had them elsewhere in their list of subjects), we chose to replace these labels with their secondary, more specific genre. \"Literature\" refers to the books that were labeled as fiction but had no other listed genre."
+  ))
 genres_page <- tabPanel(
   "Top Genres",
   sidebarLayout(
